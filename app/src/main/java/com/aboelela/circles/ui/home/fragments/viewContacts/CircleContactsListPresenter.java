4948db9d6ef24910a1448119cf1b8abc36a292fc
Aboelela48 <eng.a.aboelela@gmail.com>
2017-07-13 18:35:47 +0200

    Add circle contacts list

---
+++ app/src/main/java/com/aboelela/circles/ui/home/fragments/viewContacts/CircleContactsListPresenter.java
@@ -0,0 +1,12 @@
+package com.aboelela.circles.ui.home.fragments.viewContacts;
+
+import com.mvvm.framework.base.presenters.BasePresenter;
+
+/**
+ * Created by AboelelaA on 7/13/2017.
+ * Presenter for contacts list fragment
+ */
+
+public class CircleContactsListPresenter extends BasePresenter<CircleContactsListFragment, CircleContactsListPresenter>
+{
+}

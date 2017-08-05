3423129eb31a60969b7bbf66243af32dcda1930f
aboelela <eng.a.aboelela@gmail.com>
2017-08-06 00:15:30 +0200

    initiate swipe

---
+++ mvvmframework/src/main/java/com/mvvm/framework/utils/swipe/SwipeHorizontalDirection.java
@@ -0,0 +1,12 @@
+package com.mvvm.framework.utils.swipe;
+
+/**
+ * Created by aboelela on 05/08/17.
+ * Direction of horizontal swipe
+ */
+
+public enum SwipeHorizontalDirection
+{
+    Swipe_Left,
+    Swipe_Right
+}

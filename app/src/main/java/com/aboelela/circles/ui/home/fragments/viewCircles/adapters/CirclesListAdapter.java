00cd7dc94a30369ff91a4f87c89fb2a6c4d107a5
aboelela <eng.a.aboelela@gmail.com>
2017-08-06 00:15:30 +0200

    initiate swipe

--- app/src/main/java/com/aboelela/circles/ui/home/fragments/viewCircles/adapters/CirclesListAdapter.java
+++ app/src/main/java/com/aboelela/circles/ui/home/fragments/viewCircles/adapters/CirclesListAdapter.java
@@ -1,6 +1,5 @@
 package com.aboelela.circles.ui.home.fragments.viewCircles.adapters;
 
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -9,10 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.aboelela.circles.CirclesApplication;
 import com.aboelela.circles.R;
 import com.aboelela.circles.data.entities.Circle;
 import com.aboelela.circles.ui.home.HomeActivityMessagesHelper;
+import com.mvvm.framework.utils.LogUtil;
+import com.mvvm.framework.utils.swipe.OnSwipeListener;
+import com.mvvm.framework.utils.swipe.SwipeAnimator;
+import com.mvvm.framework.utils.swipe.SwipeDetector;
+import com.mvvm.framework.utils.swipe.SwipeHorizontalDirection;
 
 import java.util.List;
 
@@ -25,6 +28,7 @@ public class CirclesListAdapter extends RecyclerView.Adapter<CirclesListAdapter.
 {
     public CirclesListAdapter(List<Circle> circles) {
         this.circles = circles;
+        swipeDetector = new SwipeDetector();
     }
 
     @Override
@@ -34,19 +38,59 @@ public class CirclesListAdapter extends RecyclerView.Adapter<CirclesListAdapter.
                 .inflate(R.layout.list_circle_list_item, parent, false);
 
         final ViewHolder viewHolder = new ViewHolder(view);
-        viewHolder.itemView.setOnTouchListener(new View.OnTouchListener()
+        viewHolder.cardView.setTag(viewHolder);
+        viewHolder.cardView.setOnTouchListener(new View.OnTouchListener()
         {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
-                    // mark item as selected only
-                    selectedItem = viewHolder.getAdapterPosition();
-                    notifyItemChanged(viewHolder.getAdapterPosition());
-                }
-                else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
-                    // Show the contacts of this circle
-                    HomeActivityMessagesHelper.sendMessageOpenCircleContacts(circles.get(viewHolder.getAdapterPosition()));
-                }
+            public boolean onTouch(final View view, MotionEvent motionEvent) {
+
+                swipeDetector.detectSwipe(motionEvent, new OnSwipeListener()
+                {
+                    @Override
+                    public void onActionDown(float actionX, float actionY) {
+                        LogUtil.writeDebugLog(LOG_TAG, "Action Down");
+                    }
+
+                    @Override
+                    public void onActionUp(float actionX, float actionY) {
+                        LogUtil.writeDebugLog(LOG_TAG, "Action Up");
+
+                        // Show the contacts of this circle
+                        HomeActivityMessagesHelper.sendMessageOpenCircleContacts(circles.get(viewHolder.getAdapterPosition()));
+                    }
+
+                    @Override
+                    public void onSwipeStarted(float initialX, float initialY) {
+
+                    }
+
+                    @Override
+                    public void onSwipeDown(float initialX, float initialY, float currentX, float currentY, float delta) {
+
+                    }
+
+                    @Override
+                    public void onSwipeUp(float initialX, float initialY, float currentX, float currentY, float delta) {
+
+                    }
+
+                    @Override
+                    public void onSwipeLeft(float initialX, float initialY, float currentX, float currentY, float delta) {
+                        viewHolder.horizontalDirection = SwipeHorizontalDirection.Swipe_Left;
+
+                    }
+
+                    @Override
+                    public void onSwipeRight(float initialX, float initialY, float currentX, float currentY, float delta) {
+                        viewHolder.horizontalDirection = SwipeHorizontalDirection.Swipe_Right;
+                    }
+
+                    @Override
+                    public void onSwipeFinished(float initialX, float initialY) {
+                        SwipeAnimator.moveItemFullHorizontalWidth(view, ((ViewHolder)view.getTag()).horizontalDirection);
+                    }
+                });
+
                 return true;
             }
         });
@@ -58,17 +102,6 @@ public class CirclesListAdapter extends RecyclerView.Adapter<CirclesListAdapter.
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.setText(circles.get(position).getName());
-
-        // set item selected if this is the selection index
-        holder.itemView.setSelected(position == selectedItem);
-        if (holder.itemView.isSelected()) {
-            holder.itemTextView.setTextColor(ContextCompat.getColor(CirclesApplication.getInstance(),
-                    R.color.colorTextSelectionItem));
-        }
-        else {
-            holder.itemTextView.setTextColor(ContextCompat.getColor(CirclesApplication.getInstance(),
-                    R.color.colorPureBlack));
-        }
     }
 
     @Override
@@ -80,6 +113,8 @@ public class CirclesListAdapter extends RecyclerView.Adapter<CirclesListAdapter.
     {
         CardView cardView;
         TextView itemTextView;
+        View itemUndoView;
+        SwipeHorizontalDirection horizontalDirection;
 
         ViewHolder(View itemView) {
             super(itemView);
@@ -87,6 +122,7 @@ public class CirclesListAdapter extends RecyclerView.Adapter<CirclesListAdapter.
 
             cardView = (CardView) itemView.findViewById(R.id.card_view);
             itemTextView = (TextView) cardView.findViewById(R.id.circle_item_textView);
+            itemUndoView = itemView.findViewById(R.id.circle_item_delete_layout);
         }
 
         void setText(String text) {
@@ -98,6 +134,10 @@ public class CirclesListAdapter extends RecyclerView.Adapter<CirclesListAdapter.
     private List<Circle> circles;
 
     // Hold selected item index
-    private int selectedItem = -1;
+//    private int selectedItem = -1;
+
+    private SwipeDetector swipeDetector;
+
+    private static final String LOG_TAG = "CirclesListAdapter";
 
 }

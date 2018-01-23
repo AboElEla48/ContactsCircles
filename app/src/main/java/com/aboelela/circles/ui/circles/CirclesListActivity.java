7d6feaec403c8e86df30e8f2b71181b35c154044
Aboelela <Ahmed.sayed-aboelela@vodafone.com>
2018-01-23 11:41:26 +0200

    Add UI to drawable

---
+++ app/src/main/java/com/aboelela/circles/ui/circles/CirclesListActivity.java
@@ -0,0 +1,12 @@
+package com.aboelela.circles.ui.circles;
+
+import com.foureg.baseframework.ui.BaseActivity;
+
+/**
+ * Created by AboelelaA on 1/22/2018.
+ * This activity shows the list of circles
+ */
+
+public class CirclesListActivity extends BaseActivity
+{
+}

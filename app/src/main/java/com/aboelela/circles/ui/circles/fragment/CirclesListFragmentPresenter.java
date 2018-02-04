b14428791cb310ace68882e2ebbc0f01c41c4f3e
Aboelela <Ahmed.sayed-aboelela@vodafone.com>
2018-02-04 13:08:36 +0200

    Add UI layout for circle item in grid

---
+++ app/src/main/java/com/aboelela/circles/ui/circles/fragment/CirclesListFragmentPresenter.java
@@ -0,0 +1,17 @@
+package com.aboelela.circles.ui.circles.fragment;
+
+import com.foureg.baseframework.ui.BaseViewPresenter;
+
+/**
+ * Created by AboelelaA on 1/30/2018.
+ * This is the presenter class for circles list fragment
+ */
+
+class CirclesListFragmentPresenter extends BaseViewPresenter<CirclesListFragment>
+{
+    @Override
+    public void initFragmentValues() {
+        super.initFragmentValues();
+    }
+    
+}

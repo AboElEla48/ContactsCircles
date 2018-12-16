4b0530874613dcb27c091a8e8ba71dce16337dc3
Aboelela <eng.a.aboelela@gmail.com>
2018-12-16 12:57:18 +0200

    Perform delete contact operation as raw operation on specific contact from contact viewer screen

--- app/src/main/java/eg/foureg/circles/feature/contacts/view/ContactViewViewModel.kt
+++ app/src/main/java/eg/foureg/circles/feature/contacts/view/ContactViewViewModel.kt
@@ -5,19 +5,58 @@ import android.arch.lifecycle.ViewModel
 import android.graphics.Bitmap
 import eg.foureg.circles.contacts.ContactData
 import eg.foureg.circles.feature.contacts.models.ContactsModel
+import java.nio.file.Files.delete
+import android.provider.ContactsContract
+import android.net.Uri.withAppendedPath
+import android.R.attr.name
+import android.content.Context
+import android.net.Uri
+import android.provider.ContactsContract.PhoneLookup
+import io.reactivex.Observable
+
 
 class ContactViewViewModel : ViewModel() {
-    var contactName : MutableLiveData<String> = MutableLiveData()
-    var phones : MutableLiveData<List<String>> = MutableLiveData()
-    var emails : MutableLiveData<List<String>> = MutableLiveData()
-    var image : MutableLiveData<Bitmap> = MutableLiveData()
+    var contactName: MutableLiveData<String> = MutableLiveData()
+    var phones: MutableLiveData<List<String>> = MutableLiveData()
+    var emails: MutableLiveData<List<String>> = MutableLiveData()
+    var image: MutableLiveData<Bitmap> = MutableLiveData()
 
-    fun initContact(contactIndex : Int?) {
-        val contactVal : ContactData = ContactsModel.getInstance().contactsList.get(contactIndex?:0)
+    var contactIndex: Int = 0
+
+    fun initContact(contactIndex: Int?) {
+        val contactVal: ContactData = ContactsModel.getInstance().contactsList.get(contactIndex
+                ?: 0)
+
+        this.contactIndex = contactIndex!!
 
         contactName.value = contactVal.name
         phones.value = contactVal.phones
         emails.value = contactVal.emails
         image.value = contactVal.image
+    }
+
+    fun deleteContact(context: Context) {
+        Observable.fromIterable(phones.value)
+                .subscribe { phone ->
+                    val contactUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone))
+                    val cur = context.getContentResolver().query(contactUri, null, null, null, null)
+                    try {
+                        if (cur!!.moveToFirst()) {
+                            do {
+
+                                val lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY))
+                                val uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey)
+                                context.getContentResolver().delete(uri, null, null)
+
+                            } while (cur.moveToNext())
+                        }
+
+                    } catch (e: Exception) {
+                        println(e.stackTrace)
+                    } finally {
+                        cur!!.close()
+                    }
+
+                }.dispose()
     }
 }
--- app/src/main/java/eg/foureg/circles/feature/contacts/view/ContactViewerFragment.kt
+++ app/src/main/java/eg/foureg/circles/feature/contacts/view/ContactViewerFragment.kt
@@ -7,9 +7,7 @@ import android.graphics.Bitmap
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
@@ -23,9 +21,7 @@ import io.reactivex.Observable
 import kotlin.reflect.KClass
 
 /**
- * A simple [Fragment] subclass.
- * Use the [ContactViewerFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * Contact Viewer fragment
  *
  */
 class ContactViewerFragment : BaseFragment() {
@@ -66,37 +62,56 @@ class ContactViewerFragment : BaseFragment() {
 
         contactViewViewModel.phones.observe(this, Observer { phones: List<String>? ->
             Observable.fromIterable(phones)
-                    .subscribe({ phoneNumber: String ->
+                    .subscribe{ phoneNumber: String ->
                         val phoneView: View = inflater.inflate(R.layout.fragment_contact_view_phone_item, null, false)
                         val phoneTextView: TextView = phoneView.findViewById(R.id.fragment_contact_view_phone_item_phone_text_view)
                         phoneTextView.text = phoneNumber
                         contactPhonesLayout.addView(phoneView)
-                    })
+                    }
         })
 
         contactViewViewModel.emails.observe(this, Observer { emails: List<String>? ->
             Observable.fromIterable(emails)
-                    .subscribe({ email: String ->
+                    .subscribe{ email: String ->
                         val emailView: View = inflater.inflate(R.layout.fragment_contact_view_email_item, null, false)
                         val emailTextView: TextView = emailView.findViewById(R.id.fragment_contact_view_phone_item_email_text_view)
                         emailTextView.text = email
                         contactEmailsLayout.addView(emailView)
-                    })
+                    }
         })
 
-        editButton.setOnClickListener({ btn ->
+        editButton.setOnClickListener{ btn ->
             val msg = Message()
 
             msg.id = MainActivityMessages.MSG_ID_EDIT_CONTACT_DETAILS
             msg.data.put(MainActivityMessages.DATA_PARAM_CONTACT_INDEX, contactIndex as Int)
             MessageServer.getInstance().sendMessage(MainActivity::class as KClass<Any>, msg)
-        })
+        }
 
         contactViewViewModel.initContact(contactIndex)
+
+        setHasOptionsMenu(true)
 
         return view
     }
 
+    /**
+     * Inflate options menu
+     */
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.fragment_contact_viewer_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.menu_item_contact_viewer_delete -> {
+                contactViewViewModel.deleteContact(context!!)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+
+    }
 
     companion object {
         /**

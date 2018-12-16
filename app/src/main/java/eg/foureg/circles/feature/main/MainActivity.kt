27e940430094e73f4030f77329ed6d7640a51aca
Aboelela <eng.a.aboelela@gmail.com>
2018-12-16 15:11:04 +0200

    Handle contacts permissions

--- app/src/main/java/eg/foureg/circles/feature/main/MainActivity.kt
+++ app/src/main/java/eg/foureg/circles/feature/main/MainActivity.kt
@@ -1,18 +1,51 @@
 package eg.foureg.circles.feature.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import eg.foureg.circles.R
 import eg.foureg.circles.common.message.data.Message
 import eg.foureg.circles.common.ui.BaseActivity
 
+
 class MainActivity : BaseActivity() {
     val mainActivityFragmentsNavigator : MainActivityFragmentsNavigator = MainActivityFragmentsNavigator()
+    val PERMISSION_CONTACT_ID = 15
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainActivityFragmentsNavigator.setContactsListFragment(this)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ) {
+            if (checkSelfPermission(Manifest.permission.READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
+                mainActivityFragmentsNavigator.setContactsListFragment(this)
+            }
+            else {
+                requestPermissions(arrayOf(Manifest.permission.READ_CONTACTS, Manifest.permission.WRITE_CONTACTS),
+                        PERMISSION_CONTACT_ID)
+            }
+        }
+        else {
+            mainActivityFragmentsNavigator.setContactsListFragment(this)
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when(requestCode) {
+            PERMISSION_CONTACT_ID -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    mainActivityFragmentsNavigator.setContactsListFragment(this)
+                }
+                else {
+                    Toast.makeText(this, getString(R.string.txt_permission_must_be_granted), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
     override fun handleMessage(message: Message) {

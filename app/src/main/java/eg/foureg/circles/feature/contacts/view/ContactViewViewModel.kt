336dfebc772f80e6637ccbf75027ed209defb8dc
Aboelela <eng.a.aboelela@gmail.com>
2018-12-16 13:49:27 +0200

    Move contacts deletion from view model to model and contacts editor

--- app/src/main/java/eg/foureg/circles/feature/contacts/view/ContactViewViewModel.kt
+++ app/src/main/java/eg/foureg/circles/feature/contacts/view/ContactViewViewModel.kt
@@ -4,9 +4,6 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.content.Context
 import android.graphics.Bitmap
-import android.net.Uri
-import android.provider.ContactsContract
-import android.provider.ContactsContract.PhoneLookup
 import eg.foureg.circles.contacts.ContactData
 import eg.foureg.circles.feature.contacts.models.ContactsModel
 import io.reactivex.Observable
@@ -33,31 +30,6 @@ class ContactViewViewModel : ViewModel() {
     }
 
     fun deleteContact(context: Context, listener : Observable<Boolean>) {
-        Observable.fromIterable(phones.value)
-                .subscribe { phone ->
-                    val contactUri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(phone))
-                    val cur = context.getContentResolver().query(contactUri, null, null, null, null)
-                    try {
-                        if (cur!!.moveToFirst()) {
-                            do {
-
-                                val lookupKey = cur.getString(cur.getColumnIndex(ContactsContract.Contacts.LOOKUP_KEY))
-                                val uri = Uri.withAppendedPath(ContactsContract.Contacts.CONTENT_LOOKUP_URI, lookupKey)
-                                context.getContentResolver().delete(uri, null, null)
-
-                            } while (cur.moveToNext())
-
-                            // delete contact from loaded contacts list
-                            ContactsModel.getInstance().contactsList.removeAt(contactIndex)
-                        }
-
-                    } catch (e: Exception) {
-                        println(e.stackTrace)
-                    } finally {
-                        cur!!.close()
-                        listener.subscribe()
-                    }
-
-                }.dispose()
+        ContactsModel.getInstance().deleteContact(context, contactIndex, phones.value!!, listener)
     }
 }

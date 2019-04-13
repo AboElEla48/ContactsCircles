445b90762369cf5b45361dc397f27810877dc74e
Ahmed AboElEla <eng.a.aboelela@gmail.com>
2019-04-13 22:15:14 +0200

    Create feature branch for view circles list

--- app/src/main/java/eg/foureg/circles/feature/contacts/edit/ContactEditorFragment.kt
+++ app/src/main/java/eg/foureg/circles/feature/contacts/edit/ContactEditorFragment.kt
@@ -9,7 +9,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Spinner
 import com.jakewharton.rxbinding2.view.RxView
 import eg.foureg.circles.R
 import eg.foureg.circles.common.message.data.Message
@@ -17,7 +19,6 @@ import eg.foureg.circles.common.message.server.MessageServer
 import eg.foureg.circles.common.ui.BaseFragment
 import eg.foureg.circles.contacts.data.ContactData
 import eg.foureg.circles.contacts.data.ContactPhoneNumber
-import eg.foureg.circles.feature.contacts.models.ContactsModel
 import eg.foureg.circles.feature.main.content.ContentActivity
 import eg.foureg.circles.feature.main.content.ContentActivityMessages
 import io.reactivex.Observable
@@ -27,8 +28,6 @@ import kotlinx.android.synthetic.main.fragment_contact_editor.*
 import kotlinx.android.synthetic.main.fragment_contact_editor.view.*
 import kotlinx.android.synthetic.main.view_email_editor.view.*
 import kotlinx.android.synthetic.main.view_phone_number_editor.view.*
-import org.koin.android.ext.android.inject
-import kotlin.reflect.KClass
 
 
 /**

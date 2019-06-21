cdc7010084ce60216016cd24c2b38fea9acac34f
Ahmed AboElEla <eng.a.aboelela@gmail.com>
2019-06-21 14:29:49 +0200

    Call phone from circle if it has only one phone number

--- app/src/main/java/eg/foureg/circles/feature/activitycircles/contactsviewer/CircleContactsGridAdapter.kt
+++ app/src/main/java/eg/foureg/circles/feature/activitycircles/contactsviewer/CircleContactsGridAdapter.kt
@@ -1,17 +1,21 @@
 package eg.foureg.circles.feature.activitycircles.contactsviewer
 
 import android.content.Context
+import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
-import com.jakewharton.rxbinding2.view.RxView
 import eg.foureg.circles.R
 import eg.foureg.circles.contacts.data.ContactData
 import eg.foureg.circles.feature.phone.caller.PhoneCaller
+import io.reactivex.Observable
 import kotlinx.android.synthetic.main.fragment_circle_contacts_view_contact_grid_item.view.*
+import kotlin.collections.ArrayList
 
-class CircleContactsGridAdapter constructor (val context: Context, val contacts: List<ContactData>?) : BaseAdapter() {
+
+
+class CircleContactsGridAdapter constructor(val context: Context, val contacts: List<ContactData>?) : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view = LayoutInflater.from(context).inflate(R.layout.fragment_circle_contacts_view_contact_grid_item, null)
 
@@ -27,15 +31,38 @@ class CircleContactsGridAdapter constructor (val context: Context, val contacts:
         return view
     }
 
-    fun callContact(contactData: ContactData) {
-        if(contactData.phones?.size == 1) {
-            PhoneCaller.callPhone(context, contactData!!.phones?.get(0)?.phoneNumber!!)
-        }
-        else {
-//            PhoneCaller.callPhone(context, contactData!!.phones?.get(0)?.phoneNumber!!)
+    private fun callContact(contactData: ContactData) {
+        if (contactData.phones?.size == 1) {
+            PhoneCaller.callPhone(context, contactData.phones?.get(0)?.phoneNumber!!)
+        } else {
+            val strs = ArrayList<String>()
+            Observable.fromIterable(contactData.phones)
+                    .blockingSubscribe { phone ->
+                        strs.add(phone.phoneNumber)
+                    }
+
+            showPhonesDialog(strs)
         }
     }
 
+    private fun showPhonesDialog(contacts: ArrayList<String>) {
+
+        // Initialize a new instance of alert dialog builder object
+        val builder = AlertDialog.Builder(context)
+        builder.setTitle(R.string.txt_phone_numbers_list_dialog_title)
+
+        val strList = contacts.toArray(arrayOfNulls<CharSequence>(contacts.size))
+
+        builder.setItems(strList) { _, which ->
+            val selected = contacts.toArray()[which] as String
+
+            PhoneCaller.callPhone(context, selected)
+        }
+
+        builder.show()
+
+
+    }
 
 
     override fun getItem(position: Int): Any {

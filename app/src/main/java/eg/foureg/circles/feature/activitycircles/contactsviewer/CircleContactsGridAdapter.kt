af90751c7accc6db85a693589a6382135d8e94bd
Ahmed AboElEla <eng.a.aboelela@gmail.com>
2019-06-21 13:51:46 +0200

    Call phone from circle if it has only one phone number

--- app/src/main/java/eg/foureg/circles/feature/activitycircles/contactsviewer/CircleContactsGridAdapter.kt
+++ app/src/main/java/eg/foureg/circles/feature/activitycircles/contactsviewer/CircleContactsGridAdapter.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import com.jakewharton.rxbinding2.view.RxView
 import eg.foureg.circles.R
 import eg.foureg.circles.contacts.data.ContactData
+import eg.foureg.circles.feature.phone.caller.PhoneCaller
 import kotlinx.android.synthetic.main.fragment_circle_contacts_view_contact_grid_item.view.*
 
 class CircleContactsGridAdapter constructor (val context: Context, val contacts: List<ContactData>?) : BaseAdapter() {
@@ -15,8 +17,26 @@ class CircleContactsGridAdapter constructor (val context: Context, val contacts:
 
         view.fragment_circle_contacts_view_grid_item_text_view.text = contacts!!.get(position).name
 
+        view.tag = contacts.get(position)
+
+        view.setOnClickListener { view ->
+            val cData: ContactData = view.tag as ContactData
+            callContact(cData)
+        }
+
         return view
     }
+
+    fun callContact(contactData: ContactData) {
+        if(contactData.phones?.size == 1) {
+            PhoneCaller.callPhone(context, contactData!!.phones?.get(0)?.phoneNumber!!)
+        }
+        else {
+//            PhoneCaller.callPhone(context, contactData!!.phones?.get(0)?.phoneNumber!!)
+        }
+    }
+
+
 
     override fun getItem(position: Int): Any {
         return contacts!!.get(position)
